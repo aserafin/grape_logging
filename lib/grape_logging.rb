b5233d70ff7c4d11fd4adee31e333e5ca2bcdcb2
jules <jules.ivanic@gmail.com>
2015-10-21 20:10:18 +0200

    Don't forget to import the new formatter

--- lib/grape_logging.rb
+++ lib/grape_logging.rb
@@ -1,4 +1,5 @@
 require 'grape_logging/multi_io'
 require 'grape_logging/version'
 require 'grape_logging/formatters/default'
+require 'grape_logging/formatters/json'
 require 'grape_logging/middleware/request_logger'
