7997b21b17ed88da8940179a6cd17ed04c8f4857
jules <jules.ivanic@gmail.com>
2015-10-23 15:54:53 +0200

    response.body is a string. It should be serialized

--- lib/grape_logging/loggers/response.rb
+++ lib/grape_logging/loggers/response.rb
@@ -3,7 +3,7 @@ module GrapeLogging
     class Response < GrapeLogging::Loggers::Base
       def parameters(request, response)
         {
-          response: response.body
+          response: JSON.parse(response.body)
         }
       end
     end
