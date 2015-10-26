918cba5c2d56c4a2befb43bf8355865fb3f81ba3
jules <jules.ivanic@gmail.com>
2015-10-26 11:05:54 +0100

    response.body is sometimes too strange to be parse by JSON

--- lib/grape_logging/loggers/response.rb
+++ lib/grape_logging/loggers/response.rb
@@ -3,8 +3,20 @@ module GrapeLogging
     class Response < GrapeLogging::Loggers::Base
       def parameters(request, response)
         {
-          response: response.body
+          response: serialized_response_body(response)
         }
+      end
+
+      private
+      # In some cases, response.body is not parseable by JSON.
+      # For example, if you POST on a PUT endpoint, response.body is egal to """".
+      # It's strange but it's the Grape behavior...
+      def serialized_response_body(response)
+        begin
+          response.body.map{ |body| JSON.parse(body) }
+        rescue => e
+          ""
+        end
       end
     end
   end
