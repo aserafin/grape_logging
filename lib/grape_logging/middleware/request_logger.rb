5cddea00a7edaa76bf7ffa60ced91d407611e9b2
Andriy Yanko <andriy.yanko@gmail.com>
2015-11-20 17:50:14 +0200

    Support Rack::Response in logger

--- lib/grape_logging/middleware/request_logger.rb
+++ lib/grape_logging/middleware/request_logger.rb
@@ -48,7 +48,7 @@ module GrapeLogging
           method: request.request_method,
           total: format_runtime(total_runtime),
           db: format_runtime(db_runtime),
-          status: response[0]
+          status: response.is_a?(Rack::Response) ? response.status : response[0]
         )
       end
 
