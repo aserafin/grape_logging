cc35a9ad5012b83e686dc768b89d624cabc79b0a
jason-uh <jason@getupperhand.com>
2017-03-06 11:30:31 -0500

    port deep parameter logging from Badiapp/grape_logging (#37)
    * port deep parameter logging from Badiapp/grape_logging; add some specs
    
    * fix initializer for ParameterFilter
    
    * PR feedback

---
+++ spec/lib/grape_logging/loggers/request_headers_spec.rb
@@ -0,0 +1,13 @@
+require 'ostruct'
+
+describe GrapeLogging::Loggers::RequestHeaders do
+  let(:mock_request) do
+    OpenStruct.new(env: {HTTP_REFERER: 'http://example.com', HTTP_ACCEPT: 'text/plain'})
+  end
+
+  it 'strips HTTP_ from the parameter' do
+    expect(subject.parameters(mock_request, nil)).to eq({
+      headers: {'Referer' => 'http://example.com', 'Accept' => 'text/plain'}
+    })
+  end
+end
