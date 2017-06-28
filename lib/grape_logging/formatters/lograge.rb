9fb4bb9bec4f2c4061440ee531c10a7e459ccd64
Raffael Schmid <raffael@yux.ch>
2017-06-28 09:13:51 +0200

    simplify mapping

--- lib/grape_logging/formatters/lograge.rb
+++ lib/grape_logging/formatters/lograge.rb
@@ -2,21 +2,13 @@ module GrapeLogging
   module Formatters
     class Lograge
       def call(severity, datetime, _, data)
-        time = data[:time]
+        time = data.delete :time
         attributes = {
           severity: severity,
           duration: time[:total],
           db: time[:db],
-          view: time[:view],
-          headers: data[:headers].except('Host', 'Authorization'),
-          params: data[:params],
-          path: data[:path],
-          method: data[:method],
-          host: data[:host],
-          status: data[:status],
-          ip: data[:ip],
-          ua: %("#{data[:ua]}"),
-        }
+          view: time[:view]
+        }.merge(data)
         ::Lograge.formatter.call(attributes) + "\n"
       end
     end
