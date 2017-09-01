eea802b54680ba5da6f0f566db21705298493b72
Raffael Schmid <raffael@yux.ch>
2017-09-01 09:59:00 +0200

    Lograge formatter (#40)
    * add lograge formatter
    
    * simplify mapping

---
+++ lib/grape_logging/formatters/lograge.rb
@@ -0,0 +1,16 @@
+module GrapeLogging
+  module Formatters
+    class Lograge
+      def call(severity, datetime, _, data)
+        time = data.delete :time
+        attributes = {
+          severity: severity,
+          duration: time[:total],
+          db: time[:db],
+          view: time[:view]
+        }.merge(data)
+        ::Lograge.formatter.call(attributes) + "\n"
+      end
+    end
+  end
+end
